d2f2811e6b1ddd28c8426250f99435472ef04b71
Badr Moslih <113358493+Moslihbadr@users.noreply.github.com>
2023-03-26 23:34:45 +0000

    Update script.js

--- practice/script.js
+++ practice/script.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
         $("img")[i].src = $("img")[i + 1].src;
       }
     }
-    slide()
   });
   $(".left").on("click",()=>{
     const lastImage = $("img").last().attr("src");
@@ -19,28 +18,5 @@ $(document).ready(function(){
         $("img")[i].src = $("img")[i - 1].src;
       }
     }
-    slide()
   })
-  function slide() {
-    switch ($("img")[1].src) {
-      case "http://127.0.0.1:5500/practice/images/ahmed2.png":
-        $("span")[0].css('background-color', 'yellow'); 
-        break;
-      case "http://127.0.0.1:5500/practice/images/badr2.png":
-        console.log("badr")
-        break;
-      case "http://127.0.0.1:5500/practice/images/foukahi2.png":
-        console.log("foukahi")
-        break;
-      case "http://127.0.0.1:5500/practice/images/badr000.png":
-        console.log("badr2")
-        break;
-      case "http://127.0.0.1:5500/practice/images/ilyass3.png":
-        console.log("ilyas")
-        break;
-      case "http://127.0.0.1:5500/practice/images/yassine.png":
-        console.log("yassine")
-        break;
-    }
-  }
 })
